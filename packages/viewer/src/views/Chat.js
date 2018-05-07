683a5adc8abd40500820585d1d42b9a4da8d48e1
OmranAbazid <omran.1994@gmail.com>
2018-05-07 16:03:17 +0300

    fix merge issues

--- packages/viewer/src/views/Chat.js
+++ packages/viewer/src/views/Chat.js
@@ -18,21 +18,21 @@ import {
 class ChatView extends Component {
   constructor(props) {
     super(props);
-
     const { interviewees } = this.props.story;
     const interviewee = interviewees[this.findIntervieweeIndex()];
     const { story } = this.props;
-
     const localHistory = JSON.parse(
-      localStorage.getItem(`history-${story.id}-${interviewee.id}`)
+      localStorage.getItem(
+        `history-${story.id}-${story.version}-${interviewee.id}`
+      )
     );
-
     this.state = {
       actionbar: "scripted",
       currentIntervieweeId: this.props.params.chatId,
+      // history: [],
       history: localHistory || [],
       intervieweeModal: false,
-      replayCachedHistory: true, // TODO
+      replayCachedHistory: true,
       storyDetailsModal: false
     };
     this.findIntervieweeIndex = this.findIntervieweeIndex.bind(this);
@@ -43,108 +43,185 @@ class ChatView extends Component {
     this.updateHistory = this.updateHistory.bind(this);
   }
   componentDidMount() {
-    this.initHistory();
+    this.initHistory(); // init history when mounting the component
   }
   componentDidUpdate() {
-    this.initHistory(); // Init history also when switching between interviewees
+    this.initHistory(); // init history also when switching between interviewees
   }
   onHistoryUpdate() {
+    // grab necessary info
     const { history } = this.state;
-    const { interviewees } = this.props.story;
-    const { storyline } = interviewees[this.findIntervieweeIndex()];
-    const thisHistoryItem = history[history.length - 1];
-    const thisHistoryItemI = thisHistoryItem ? thisHistoryItem.i : 0;
-    const thisItemType = thisHistoryItem.type;
+    const { story } = this.props;
+    const { storyline } = story.interviewees[this.findIntervieweeIndex()];
 
-    const isCurrentNotTheLastItem = thisHistoryItemI < storyline.length - 1;
-    const isCurrentTheLastItem = thisHistoryItemI === storyline.length - 1;
+    // detect conditions
+    const thisItem = history[history.length - 1];
+    const thisItemIndex = thisItem ? thisItem.i : 0;
+    const isThisItemLast = thisItemIndex === storyline.length - 1;
 
-    if (isCurrentNotTheLastItem) {
-      const nextHistoryItem = storyline[thisHistoryItemI + 1];
-      const secondNextHistoryItem = storyline[thisHistoryItemI + 2];
-      const nextItemRole = nextHistoryItem.role;
+    if (!isThisItemLast) {
+      // detect more conditions
+      const thisItemType = thisItem ? thisItem.type : null;
 
-      const isItIntervieweesTurn = nextItemRole === "interviewee";
-      const isItUsersTurn = nextItemRole === "user";
+      const prevItem = history[history.length - 2];
+      const nextItem = storyline[thisItemIndex + 1];
+      const secondNextItem = storyline[thisItemIndex + 2];
 
-      if (thisItemType === "init") {
-        if (isItIntervieweesTurn) {
-          // wait 1050 for the prev bubble to end preloading
-          setTimeout(() => this.updateHistory("followup"), 1050);
+      const prevRole = prevItem ? prevItem.role : null;
+      const nextRole = nextItem ? nextItem.role : null;
+      const secondNextRole = secondNextItem ? secondNextItem.role : null;
+
+      const isIntervieweesTurn = nextRole === "interviewee";
+      const isUsersTurn = nextRole === "user";
+      const willIntervieweeCarryOn = secondNextRole === "interviewee";
+
+      if (thisItemType === "init" && isIntervieweesTurn) {
+        setTimeout(() => this.updateHistory("followup"), 1050);
+      } else if (thisItemType === "ignore") {
+        if (isIntervieweesTurn && willIntervieweeCarryOn) {
+          this.updateHistory("skip");
+        } else if (isUsersTurn) {
+          this.setState({ actionbar: "scripted" });
+        } else {
+          this.updateHistory("followup");
+        }
+      } else if (thisItemType === "explore") {
+        if (isIntervieweesTurn) {
+          this.updateHistory("followup");
+        } else if (isUsersTurn) {
+          this.setState({ actionbar: "scripted" });
         }
         return null;
       } else if (thisItemType === "followup") {
-        if (isItIntervieweesTurn) {
-          // wait 1050 for the prev bubble to end preloading
+        // detect more conditions
+        const isExploring = prevRole === "user" && prevItem.value === 1;
+        if (isExploring && isIntervieweesTurn && willIntervieweeCarryOn) {
+          setTimeout(() => this.updateHistory("skip"), 1500);
+        } else if (
+          isExploring &&
+          isIntervieweesTurn &&
+          !willIntervieweeCarryOn
+        ) {
+          this.updateHistory("fastfwd");
+        } else if (isIntervieweesTurn) {
           setTimeout(() => this.updateHistory("followup"), 1500);
-        } else if (isItUsersTurn) {
+        } else if (isUsersTurn) {
           setTimeout(() => this.setState({ actionbar: "scripted" }), 2000);
         }
-        return null;
-      } else if (thisItemType === "emoji") {
-        return null;
-      } else if (thisItemType === "ignore") {
-        const areNextTwoByInterviewee =
-          isItIntervieweesTurn && secondNextHistoryItem.role === "interviewee";
-        const isNextScriptedItemByUser = nextHistoryItem.role === "user";
-        if (areNextTwoByInterviewee) {
-          this.updateHistory("skip");
-        } else if (isNextScriptedItemByUser) {
-          this.setState({ actionbar: "scripted" });
-        } else {
-          this.updateHistory("followup");
-        }
-        return null;
-      } else if (thisItemType === "explore") {
-        const isNextScriptedItemByUser = nextHistoryItem.role === "user";
-        if (isNextScriptedItemByUser) {
-          this.setState({ actionbar: "scripted" });
-        } else {
-          this.updateHistory("followup");
-        }
-        return null;
-      } else if (thisItemType === "switchTo") {
-        return null;
-      } else if (thisItemType === "nvm") {
-        return null;
-      } else if (thisItemType === "quit") {
-        return null;
       }
       return null;
-    } else if (isCurrentTheLastItem) {
+    } else if (isThisItemLast) {
       this.updateHistory("quit");
     }
     return null;
   }
+  updateHistory(type, payload) {
+    // hide actionbar till onHistoryUpdate will trigger another updateHistory loop that will enable it
+    this.setState({ actionbar: null });
 
+    // grab necessary info
+    const { history } = this.state;
+    const thisItem = history[history.length - 1];
+
+    // write history:
+    if (type === "ignore" || type === "explore") {
+      this.setState({ actionbar: null });
+      const action = {
+        i: history.length > 0 ? thisItem.i + 1 : 0,
+        role: "user",
+        type,
+        value: payload
+      };
+      history.push(action);
+    } else if (type === "followup") {
+      const followup = {
+        i: thisItem.i + 1,
+        role: "interviewee",
+        type: "followup"
+      };
+      history.push(followup);
+    } else if (type === "fastfwd") {
+      this.setState({ actionbar: "scripted" });
+      const fastfwd = {
+        i: thisItem.i + 1,
+        type: "fastfwd"
+      };
+      history.push(fastfwd);
+    } else if (type === "skip") {
+      this.setState({ actionbar: null });
+      const skip = {
+        i: thisItem.i + 2,
+        role: "interviewee",
+        type: "followup"
+      };
+      history.push(skip);
+    } else if (type === "emoji") {
+      this.setState({ actionbar: "scripted" });
+      const emoji = {
+        i: thisItem.i,
+        role: "user",
+        type: "emoji",
+        value: payload
+      };
+      history.push(emoji);
+    } else if (type === "switchTo") {
+      this.setState({ actionbar: "scripted" });
+      const switchTo = {
+        i: thisItem.i - 1,
+        role: "system",
+        type: "switchTo"
+      };
+      history.push(switchTo);
+    } else if (type === "nvm") {
+      this.setState({ actionbar: "scripted" });
+      history.splice(-1, 1);
+    } else if (type === "quit") {
+      const quit = {
+        role: "system",
+        type: "quit"
+      };
+      history.push(quit);
+    }
+
+    // save updated history in localStorage unless in switch interviewee loop
+    if (type !== "nvm" && type !== "switchTo") {
+      const { story } = this.props;
+      const { interviewees } = story;
+      const interviewee = interviewees[this.findIntervieweeIndex()];
+      localStorage.setItem(
+        `history-${story.id}-${story.version}-${interviewee.id}`,
+        JSON.stringify(history)
+      );
+    }
+
+    // update history to re-render storyline, then fire onHistoryUpdate
+    this.setState({ history }, () => this.onHistoryUpdate());
+  }
   switchChat(chatId) {
     const { story } = this.props;
-
     // get the other interviewee’s history saved in localStorage
     const localHistory = JSON.parse(
-      localStorage.getItem(`history-${story.id}-${chatId}`)
+      localStorage.getItem(`history-${story.id}-${story.version}-${chatId}`)
     );
     this.setState({
       actionbar: "scripted",
       currentIntervieweeId: chatId,
+      // history: []
       history: localHistory || []
     });
     this.props.router.push(`/${story.id}/chat/${chatId}`);
   }
-
   toggleToolbar(toolbar) {
     this.setState({ [toolbar]: !this.state[toolbar] });
   }
   toggleModal(modal) {
     this.setState({ [modal]: !this.state[modal] });
   }
-
   findIntervieweeIndex() {
     const { interviewees } = this.props.story;
     const { chatId } = this.props.params;
-    return interviewees.findIndex(item => item.id === chatId);
+    return interviewees.findIndex((item) => item.id === chatId);
   }
-
   initHistory() {
     const { interviewees } = this.props.story;
     const { storyline } = interviewees[this.findIntervieweeIndex()];
@@ -162,83 +239,6 @@ class ChatView extends Component {
       );
     }
     return null;
-  }
-
-  updateHistory(type, payload) {
-    // hide actionbar till onHistoryUpdate will trigger another updateHistory loop that will enable it
-    this.setState({ actionbar: null });
-
-    // grab necessary information
-    const { history } = this.state;
-    const { story } = this.props;
-    const { interviewees } = story;
-    const interviewee = interviewees[this.findIntervieweeIndex()];
-    const thisHistoryItem = history[history.length - 1];
-
-    // local redux-y way of handling updateHistory logic
-    if (type === "switchTo") {
-      this.setState({ actionbar: "scripted" });
-      const switchTo = {
-        i: thisHistoryItem.i - 1,
-        role: "system",
-        type: "switchTo"
-      };
-      history.push(switchTo);
-    } else if (type === "nvm") {
-      this.setState({ actionbar: "scripted" });
-      history.splice(-1, 1);
-    } else if (type === "emoji") {
-      this.setState({ actionbar: "scripted" });
-      const emoji = {
-        i: thisHistoryItem.i,
-        role: "user",
-        type: "emoji",
-        value: payload
-      };
-      history.push(emoji);
-    } else if (type === "quit") {
-      const quit = {
-        role: "system",
-        type: "quit"
-      };
-      history.push(quit);
-    } else if (type === "skip") {
-      this.setState({ actionbar: null });
-      const skip = {
-        i: thisHistoryItem.i + 2,
-        role: "interviewee",
-        type: "followup"
-      };
-      history.push(skip);
-    } else if (type === "ignore" || type === "explore") {
-      this.setState({ actionbar: null });
-      const action = {
-        i: history.length > 0 ? thisHistoryItem.i + 1 : 0,
-        role: "user",
-        type,
-        value: payload
-      };
-      history.push(action);
-    } else if (type === "followup") {
-      const followup = {
-        i: thisHistoryItem.i + 1,
-        role: "interviewee",
-        type: "followup"
-      };
-      history.push(followup);
-    }
-
-    // save updated history in localStorage unless in switch interviewee loop
-    if (type !== "nvm" && type !== "switchTo") {
-      localStorage.setItem(
-        `history-${story.id}-${interviewee.id}`,
-        JSON.stringify(history)
-      );
-    }
-
-    // update history state to re-render storyline
-    // assume history is up-to-date, fire this.onHistoryUpdate()
-    this.setState({ history }, () => this.onHistoryUpdate());
   }
 
   render() {
@@ -273,7 +273,7 @@ class ChatView extends Component {
     const hideActionbarSatellites =
       !isNvmBubble() && !isLastBubble() && hasHistory;
 
-    const getCurrentScriptActions = arr =>
+    const getCurrentScriptActions = (arr) =>
       arr.map((action, i) => {
         if (action.enabled) {
           return (
@@ -300,12 +300,12 @@ class ChatView extends Component {
 
       if (hasHistory) {
         const thisHistoryItem = history[history.length - 1];
-        const thisHistoryItemI = thisHistoryItem.i;
-        const nextHistoryItem = storyline[thisHistoryItemI + 1];
+        const thisItemIndex = thisHistoryItem.i;
+        const nextItem = storyline[thisItemIndex + 1];
 
         const isLastBubbleSwitchTo = thisHistoryItem.type === "switchTo";
         const isTheVeryLastBubble =
-          thisHistoryItemI === storyline.length - 1 ||
+          thisItemIndex === storyline.length - 1 ||
           thisHistoryItem.type === "quit";
 
         if (isTheVeryLastBubble || isActiveActionbarRunaway) {
@@ -318,15 +318,15 @@ class ChatView extends Component {
             />
           );
         }
-        const isNextHistoryItemUser = nextHistoryItem
-          ? nextHistoryItem.role === "user"
+        const isNextHistoryItemUser = nextItem
+          ? nextItem.role === "user"
           : false;
         if (isNextHistoryItemUser && isActiveActionbarEmot) {
           return <EmoActions updateHistory={this.updateHistory} />;
         } else if (isLastBubbleSwitchTo) {
           return <NvmActions updateHistory={this.updateHistory} />;
         } else if (isNextHistoryItemUser && isActiveActionbarScripted) {
-          return getCurrentScriptActions(nextHistoryItem.content);
+          return getCurrentScriptActions(nextItem.content);
         }
         return null;
       } else if (userStarts && isActiveActionbarScripted) {
