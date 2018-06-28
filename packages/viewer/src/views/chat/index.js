fb6be9dcd995d30da654257fb43a79c105997a50
Piotr F <piotr@piotrf.pl>
2018-06-28 16:45:21 +0200

    Cleanup.

--- packages/viewer/src/views/chat/index.js
+++ packages/viewer/src/views/chat/index.js
@@ -1,7 +1,6 @@
 // misc
 export { default as EmoActions } from "./EmoActions";
 export { default as ChatActions } from "./ChatActions";
-export { default as NvmActions } from "./NvmActions";
 export { default as Page } from "./Page";
 export { default as PageBody } from "./PageBody";
 export { default as PageFoot } from "./PageFoot";
