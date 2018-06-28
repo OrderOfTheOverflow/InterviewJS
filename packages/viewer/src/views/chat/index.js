0446abe9556c361ed5562725a80c4d76317ce11f
Piotr F <piotr@piotrf.pl>
2018-06-28 17:29:35 +0200

    Revert "Cleanup."
    This reverts commit fb6be9dcd995d30da654257fb43a79c105997a50.

--- packages/viewer/src/views/chat/index.js
+++ packages/viewer/src/views/chat/index.js
@@ -1,6 +1,7 @@
 // misc
 export { default as EmoActions } from "./EmoActions";
 export { default as ChatActions } from "./ChatActions";
+export { default as NvmActions } from "./NvmActions";
 export { default as Page } from "./Page";
 export { default as PageBody } from "./PageBody";
 export { default as PageFoot } from "./PageFoot";
