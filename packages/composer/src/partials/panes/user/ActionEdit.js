58daf6116cfb59f8bc624657251e33a2ca0432f7
Piotr F <piotr@piotrf.pl>
2018-07-09 14:43:42 +0200

    Add PaneTabs to ActionEdit.

--- packages/composer/src/partials/panes/user/ActionEdit.js
+++ packages/composer/src/partials/panes/user/ActionEdit.js
@@ -1,18 +1,32 @@
 import { array, bool, oneOfType, node, string } from "prop-types";
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 
-import {} from "interviewjs-styleguide";
+import { Icon, PaneTabs, PaneTab, radius } from "interviewjs-styleguide";
 
 import { GLOBALS, USER_ACTIONS } from "../../../options";
 
 const ActionEditEl = styled.div`
+  border-radius: ${radius.l} ${radius.l} ${radius.n} ${radius.n};
+  overflow: hidden;
+  width: 100%;
+
   ${({ locked }) =>
     locked
       ? `
     cursor: pointer;
   `
       : ``};
+
+  ${PaneTabs} {
+    background: transparent;
+    position: relative;
+    top: -1px;
+    & > * {
+      background: transparent;
+      flex-basis: ${100 / 6}%;
+    }
+  }
 `;
 
 export default class ActionEdit extends Component {
@@ -22,10 +36,33 @@ export default class ActionEdit extends Component {
   }
   render() {
     const { children, locked } = this.props;
-    const getUnlockedContent = () => <div>Unlocked</div>;
+    const getUnlockedContent = () => (
+      <Fragment>
+        <PaneTabs>
+          <PaneTab active>
+            <Icon name="text" size="x" />
+          </PaneTab>
+          <PaneTab>
+            <Icon name="link" size="x" />
+          </PaneTab>
+          <PaneTab>
+            <Icon name="image" size="x" />
+          </PaneTab>
+          <PaneTab>
+            <Icon name="embed" size="x" />
+          </PaneTab>
+          <PaneTab>
+            <Icon name="map" size="x" />
+          </PaneTab>
+          <PaneTab>
+            <Icon name="media" size="x" />
+          </PaneTab>
+        </PaneTabs>
+      </Fragment>
+    );
     return (
       <ActionEditEl {...this.props}>
-        {locked ? children : getUnlockedContent()}
+        {!locked ? children : getUnlockedContent() /* TODO: INVERT IF CLAUSE */}
       </ActionEditEl>
     );
   }
