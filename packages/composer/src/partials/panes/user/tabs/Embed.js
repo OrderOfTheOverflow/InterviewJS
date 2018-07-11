0c3e9dc704ab558295a22e59c1f8e28eba0b76a8
Piotr F <piotr@piotrf.pl>
2018-07-11 16:15:28 +0200

    Add proper Embed tab.

--- packages/composer/src/partials/panes/user/tabs/Embed.js
+++ packages/composer/src/partials/panes/user/tabs/Embed.js
@@ -1,18 +1,69 @@
-import {} from "prop-types";
+import { func, shape, string } from "prop-types";
 import React, { Component } from "react";
 
-import { Container } from "interviewjs-styleguide";
+import {
+  Container,
+  FormItem,
+  Label,
+  Legend,
+  TextInput
+} from "interviewjs-styleguide";
+
+import { filterIframe } from "../../../../util/IframeSanitizer";
+// import { GLOBALS, USER_ACTIONS } from "../../../../options";
 
 export default class EmbedTab extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    const clean = filterIframe(value);
+
+    const draft = {
+      ...this.props.draft,
+      [name]: value
+    };
+
+    if (
+      clean.toLowerCase().startsWith("<iframe") &&
+      clean.toLowerCase().includes("src=") &&
+      clean.toLowerCase().endsWith("></iframe>")
+    ) {
+      console.log("passed validation");
+      this.props.updateDraft("embed", draft);
+    }
+    return console.log("code not valid");
+  };
   render() {
-    return <Container padded>Hello Embed Tab</Container>;
+    const { value } = this.props.draft;
+    return (
+      <Container padded>
+        <FormItem>
+          <Label>Embed iframe</Label>
+          <TextInput
+            area
+            name="value"
+            onChange={(e) => this.handleChange(e)}
+            placeholder="Insert an iframe to display web content directly into your chat"
+            required
+            rows={10}
+            type="url"
+            value={value}
+          />
+          <Legend tip="Insert an iframe to display web content directly in your chat. Click the share icon to see if an ‘embed code’ is available. Then copy and paste.">
+            i
+          </Legend>
+        </FormItem>
+      </Container>
+    );
   }
 }
 
-EmbedTab.propTypes = {};
+EmbedTab.propTypes = {
+  updateDraft: func.isRequired
+};
 
 EmbedTab.defaultProps = {};
