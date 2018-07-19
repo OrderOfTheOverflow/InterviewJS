6462a17b07cdc3660b08b10bb10245e6b6b927f9
Piotr F <piotr@piotrf.pl>
2018-07-19 16:10:03 +0200

    More tour styles.

--- packages/styleguide/catalog/components/tours/Tour.js
+++ packages/styleguide/catalog/components/tours/Tour.js
@@ -3,63 +3,69 @@ import Joyride from "react-joyride";
 import React from "react";
 
 import { color, font } from "../../../utils";
+import "./tour.css";
 
 const Tour = (props) => {
   const buttonStyle = {
-    borderColor: color.blueM,
+    borderColor: color.flareBlk,
     borderRadius: "30px",
     borderStyle: "solid",
     borderWidth: "1px",
     fontFamily: font.serif,
-    fontSize: "14px",
+    fontSize: "13px",
     lineHeight: "1.2em",
     padding: "5px 10px"
   };
 
   const secButtonStyle = {
     background: "transparent",
-    color: color.blueM
+    color: color.white
   };
 
   const priButtonStyle = {
-    background: color.blueM,
-    color: color.white
+    background: color.white,
+    color: color.greenHD
   };
 
   const styles = {
     options: {
-      arrowColor: color.greyWt,
-      backgroundColor: color.greyWt,
+      arrowColor: color.greenHD,
+      backgroundColor: color.greenHD,
       beaconSize: 36
     },
     beacon: {},
     beaconInner: {
-      background: color.greenM
+      background: color.greenHD
     },
     beaconOuter: {
-      borderColor: color.greenM,
+      borderColor: color.greenHD,
       background: color.flareM
     },
     tooltip: {
+      animation: "pulsate-fwd 1.5s ease-in-out infinite both",
       borderRadius: "10px",
-      padding: "20px"
+      padding: "15px",
+      width: "240px"
     },
     tooltipContainer: {},
     tooltipTitle: {
-      color: color.blueBlk,
+      color: color.white,
+      fontFamily: font.serif,
+      fontSize: "14px",
       marginBottom: "10px",
       padding: "0",
       textAlign: "left"
     },
     tooltipContent: {
+      color: color.flareBlk,
       fontFamily: font.serif,
-      color: color.greyBlk,
+      fontSize: "14px",
       marginTop: "10px",
       padding: "0",
       textAlign: "left"
     },
     tooltipFooter: {
-      marginTop: "20px"
+      marginTop: "15px"
     },
     buttonNext: {
       ...buttonStyle,
@@ -69,7 +75,9 @@ const Tour = (props) => {
       ...buttonStyle,
       ...secButtonStyle
     },
-    buttonClose: {},
+    buttonClose: {
+      display: "none"
+    },
     buttonSkip: {
       ...buttonStyle,
       ...secButtonStyle
@@ -77,18 +85,17 @@ const Tour = (props) => {
     overlay: { background: color.flareM },
     overlayLegacy: { background: color.flareM },
     spotlight: {
+      animation: "blink 1.5s ease-in-out infinite both",
       borderRadius: "10px",
       boxShadow: `
         0px 2px 5px ${color.shadowHL},
         0px -2px 5px ${color.shadowHL}
       `,
-      border: `3px solid ${color.blueBlk}`
+      border: `3px solid ${color.greenBlk}`
     },
     spotlightLegacy: {},
     floater: {
-      arrow: {
-        color: "#f7f7f7"
-      },
+      arrow: {},
       tooltip: {}
     }
   };
