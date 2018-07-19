42e7a5bb3c70c4a4cfd60db540379d003d5c9e64
Piotr F <piotr@piotrf.pl>
2018-07-20 00:30:01 +0200

    evalConditions.

--- packages/composer/src/partials/misc/ComposerHelp.js
+++ packages/composer/src/partials/misc/ComposerHelp.js
@@ -1,4 +1,4 @@
-// import { object } from "prop-types";
+import { bool, shape } from "prop-types";
 import React, { Component } from "react";
 
 import { Tour } from "interviewjs-styleguide";
@@ -10,18 +10,16 @@ const STEPS = [
     target: ".jr-step-00",
     placement: "right",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
-      close: "Done!"
+      close: "Next"
     }
   },
   {
     // 1
-    title: "Now select a meaningful quote.",
+    title: "Select some text to highlight a meaningful quote.",
     target: ".jr-step-01",
     placement: "right-start",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
       close: "Got it"
     }
@@ -32,53 +30,48 @@ const STEPS = [
     target: ".jr-step-02",
     placement: "top",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
       close: "Looking good"
     }
   },
   {
     // 3
-    title: "Now script a question leading to selected quote.",
+    title: "Script a question leading to selected quote.",
     target: ".jr-step-03",
     placement: "left",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
-      close: "Done!"
+      close: "Done"
     }
   },
   {
     // 4
-    title: "You can preview those user actions here.",
+    title: "Double-check scripted user actions here.",
     target: ".jr-step-04",
     placement: "top",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
-      close: "Nice!"
+      close: "Looking good"
     }
   },
   {
     // 5
-    title: "Now add them to the storyline.",
+    title: "Add scripted user actions to the storyline.",
     target: ".jr-step-05",
     placement: "left",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
-      close: "Done!"
+      close: "Done"
     }
   },
   {
     // 6
-    title: "You can now add previously selected quote to the storyline.",
+    title: "Add previously selected quote to the storyline.",
     target: ".jr-step-06",
     placement: "right",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
-      close: "Great!"
+      close: "Done"
     }
   },
   {
@@ -88,9 +81,8 @@ const STEPS = [
     target: ".jr-step-07",
     placement: "right",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
-      close: "Got it"
+      close: "Gotcha"
     }
   },
   {
@@ -99,7 +91,6 @@ const STEPS = [
     target: ".jr-step-08",
     placement: "center",
     disableBeacon: true,
-    showSkipButton: false,
     locale: {
       close: "Thanks"
     }
@@ -110,113 +101,68 @@ export default class ReactiveHelp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      run: true,
-      // stepIndex: 0,
+      run: false,
+      stepIndex: 0,
       steps: STEPS
     };
-    // this.callback = this.callback.bind(this);
-    // this.ctrlTour = this.ctrlTour.bind(this);
-    // this.evalConditions = this.evalConditions.bind(this);
+    this.evalConditions = this.evalConditions.bind(this);
   }
 
-  // componentDidMount() {
-  //   this.evalConditions(this.props.conditions);
-  // }
+  componentDidMount() {
+    setTimeout(this.evalConditions, 2000);
+  }
 
-  // evalConditions(conditions) {
-  //   console.group("Eval conditions");
-  //   console.log(conditions);
-  //   console.groupEnd();
-  //
-  //   const ruleset0 = !conditions.hasTranscript;
-  //   const ruleset1 =
-  //     conditions.hasTranscript && !conditions.hasIntervieweeDraft;
-  //   const ruleset2 = conditions.hasTranscript && conditions.hasIntervieweeDraft;
-  //   const ruleset3 = ruleset1 && conditions.hasIntervieweeDraft;
-  //   const ruleset4 = ruleset3 && !conditions.hasUserDraft;
-  //   const ruleset5 = ruleset3 && conditions.hasUserDraft;
-  //   const ruleset6 = ruleset4 && conditions.isLastBubbleUsers;
-  //   // const ruleset7 = ruleset4 && conditions.isLastBubbleUsers;
-  //   // const ruleset8 = ruleset4 && conditions.isLastBubbleUsers;
-  //
-  //   if (ruleset0) {
-  //     this.setState({ run: true, stepIndex: 0 });
-  //   } else if (ruleset1) {
-  //     this.setState({ run: true, stepIndex: 1 });
-  //   } else if (ruleset2) {
-  //     this.setState({ run: true, stepIndex: 2 });
-  //   } else if (ruleset3) {
-  //     this.setState({ run: true, stepIndex: 3 });
-  //   } else if (ruleset4) {
-  //     this.setState({ run: true, stepIndex: 4 });
-  //   } else if (ruleset5) {
-  //     this.setState({ run: true, stepIndex: 5 });
-  //   } else if (ruleset6) {
-  //     this.setState({ run: true, stepIndex: 6 });
-  //   }
-  //   return null;
-  // }
+  evalConditions() {
+    const {
+      hasTranscript,
+      hasIntervieweeDraft,
+      hasUserDraft,
+      isLastBubbleUsers,
+      isLastBubbleInterviewees
+    } = this.props.conditions;
 
-  // ctrlTour(action: string, target: number) {
-  //   switch (action) {
-  //     case "stop":
-  //       return this.setState({ run: false });
-  //     case "start":
-  //       return this.setState({ run: true });
-  //     case "fwd":
-  //       return this.setState({ stepIndex: target });
-  //     default:
-  //       return null;
-  //   }
-  // }
+    console.group("Eval conditions");
+    console.log(this.props.conditions);
+    console.groupEnd();
 
-  // callback = (data) => {
-  //   const { action, index, type } = data;
-  //   const { action } = data;
-  //   const { stepIndex } = this.state;
-  //   const { conditions } = this.props;
-  //
-  //   console.group("Joyride action, bitch!");
-  //   console.log(data);
-  //   console.groupEnd();
-  //
-  //   if (action === "close" && action === "next") {
-  //     switch (stepIndex) {
-  //       case 0:
-  //         return conditions.hasTranscript
-  //           ? this.setState({ stepIndex: 1, run: true })
-  //           : console.log("doesn’t have a transcript");
-  //       case 1:
-  //         return console.log("stepIndex: 1");
-  //       default:
-  //         return null;
-  //     }
-  //   }
-  //
-  //   return null;
-  //
-  //   if (action === "close") {
-  //     this.setState({ run: false });
-  //   } else if (action === "prev" && this.state.stepIndex < 0) {
-  //     this.setState({ stepIndex: this.state.stepIndex - 1 });
-  //   }
-  // };
+    const ruleset0 = !hasTranscript;
+    const ruleset1 = hasTranscript && !hasIntervieweeDraft;
+    const ruleset2 = hasTranscript && hasIntervieweeDraft;
+    const ruleset3 = ruleset1 && hasIntervieweeDraft;
+    const ruleset4 = ruleset3 && !hasUserDraft;
+    const ruleset5 = ruleset3 && hasUserDraft;
+    const ruleset6 = ruleset4 && isLastBubbleUsers;
+    const ruleset7 = isLastBubbleInterviewees;
+    // const ruleset8 = ruleset4 && conditions.isLastBubbleUsers;
+
+    if (ruleset0) {
+      this.setState({ run: true, stepIndex: 0 });
+    } else if (ruleset1) {
+      this.setState({ run: true, stepIndex: 1 });
+    } else if (ruleset2) {
+      this.setState({ run: true, stepIndex: 2 });
+    } else if (ruleset3) {
+      this.setState({ run: true, stepIndex: 3 });
+    } else if (ruleset4) {
+      this.setState({ run: true, stepIndex: 4 });
+    } else if (ruleset5) {
+      this.setState({ run: true, stepIndex: 5 });
+    } else if (ruleset6) {
+      this.setState({ run: true, stepIndex: 6 });
+    } else if (ruleset7) {
+      this.setState({ run: true, stepIndex: 7 });
+    }
+    return null;
+  }
 
   render() {
-    // const { stepIndex, steps, run } = this.state;
-    const { steps, run } = this.state;
-
-    // console.group("Tour Conditions");
-    // console.log(props.conditions);
-    // console.groupEnd();
-
+    const { stepIndex, steps, run } = this.state;
     return (
       <Tour
-        // callback={this.callback}
         continuous={false}
         disableCloseOnEsc
-        disableOverlayClose
         disableOverlay={false}
+        disableOverlayClose
         hideBackButton={false}
         locale={{
           back: "Back",
@@ -225,18 +171,23 @@ export default class ReactiveHelp extends Component {
           next: "Done!"
         }}
         run={run}
-        steps={steps}
         showProgress={false}
-        showSkipButton
+        showSkipButton={false}
         spotlightClicks
-        // stepIndex={stepIndex}
+        steps={steps}
       />
     );
   }
 }
 
 ReactiveHelp.propTypes = {
-  // conditions: object.isRequired
+  conditions: shape({
+    hasIntervieweeDraft: bool,
+    hasTranscript: bool,
+    hasUserDraft: bool,
+    isLastBubbleInterviewees: bool,
+    isLastBubbleUsers: bool
+  }).isRequired
 };
 
 ReactiveHelp.defaultProps = {};
